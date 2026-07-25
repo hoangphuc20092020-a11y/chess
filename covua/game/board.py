5ae3440379d111b90a21e97bd50e34533af6acd2
hoangphuc20092020-a11y <hoangphuc20092020@gmail.com>
2026-07-25 10:32:49 +0700

    25/07/2026

---
+++ covua/game/board.py
@@ -0,0 +1,20 @@
+import numpy as np
+from typing import Optional
+# màu cờ
+WHITE = 'w'
+BLACK = 'b'
+# loại quân
+KING = 'K'
+QUEEN = 'Q'
+ROOK = 'R'
+BISHOP = 'B'
+KNIGHT = 'N'
+PAWN = 'P'
+# ký hiệu Unicode để in ra terminal
+PIECE_UNICODE = {
+    'wK': '♔', 'wQ': '♕', 'wR': '♖', 'wB': '♗', 'wN': '♘', 'wP': '♙',
+    'bK': '♚', 'bQ': '♛', 'bR': '♜', 'bB': '♝', 'bN': '♞', 'bP': '♟',  
+}
+
+
+
